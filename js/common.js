57782c0ef8cd95ac7e82ea1f51e4f16a8063bdf6
ITkah <ilatkacenko132@gmail.com>
2020-09-14 18:36:39 +0300

    header-fix

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+    $(".call_menu").on("click", function(e){
+        e.preventDefault();
+        $("nav").addClass("active_menu");
+    });
+    $(".close_menu").on("click", function(e){
+        e.preventDefault();
+        $("nav").removeClass("active_menu");
+    });
+    $(".custom_link_item").on("click", function(e){
+        $(this).toggleClass("active_custom");
+        $(".custom_hide_link").slideToggle(150);
+    });
 });
