417d7b7eeb74ee54fd80b690dfd7f12db6250e63
ITkah <ilatkacenko132@gmail.com>
2020-09-24 15:04:44 +0300

    add pages

--- js/common.js
+++ js/common.js
@@ -1,14 +1,26 @@
-$(document).ready(function(){
-    $(".call_menu").on("click", function(e){
+$(document).ready(function() {
+    $(".call_menu").on("click", function(e) {
         e.preventDefault();
         $("nav").addClass("active_menu");
     });
-    $(".close_menu").on("click", function(e){
+    $(".close_menu").on("click", function(e) {
         e.preventDefault();
         $("nav").removeClass("active_menu");
     });
-    $(".custom_link_item").on("click", function(e){
+
+    $(".custom_link_item").on("click", function(e) {
         $(this).toggleClass("active_custom");
+        $("nav").toggleClass("scroll_element");
         $(".custom_hide_link").slideToggle(150);
+    });
+
+    $(".menu_head li").each(function() {
+        this_li = $(this);
+        if ($(this_li).find("ul").length > 0) {
+            $(this_li).append('<p class="arrow_mob"></p>')
+        }
+    });
+    $(".arrow_mob").on("click", function(e) {
+        $(this).siblings(".sub-menu").slideToggle(100);
     });
 });
